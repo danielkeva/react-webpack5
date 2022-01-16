562dfbb6fd8e117d3bcb00a23a442e07a015bd53
Daniel Keva <danielk@webiya.co.il>
2022-01-16 10:51:41 +0200

    add webpack5 configuration

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const { EnvironmentPlugin } = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
+module.exports = (_, { mode }) => {
+  const production = mode === 'production'
+
+  return {
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      devMiddleware: { stats: 'errors-warnings' }
+    },
+    devtool: production ? undefined : 'source-map',
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: [
+            production ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: { localIdentName: production ? '[hash:base64:6]' : '[name]_[local]-[hash:base64:5]' },
+                importLoaders: 2
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          type: 'asset/resource',
+          generator: { filename: 'images/[name].[hash][ext]' }
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: { filename: 'fonts/[name].[hash][ext]' }
+        }
+      ]
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'js/[name].[contenthash].js',
+      clean: true
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        minSize: 10000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: module => (module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/) || [])[1]
+          }
+        }
+      },
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin({
+          minimizerOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }]
+          }
+        })
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' }),
+      new ESLintPlugin(),
+      new HtmlPlugin({ template: 'public/index.html' }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+            globOptions: { ignore: '**/index.html' }
+          }
+        ]
+      })
+    ]
+  }
+}
